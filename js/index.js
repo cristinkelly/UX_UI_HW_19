ea1feab379609496a7d03b9fd4468c03dc07335e
cristinkelly <115126698+cristinkelly@users.noreply.github.com>
2023-04-12 23:16:15 -0500

    4.12
    Corrected double image folder in the js

--- js/index.js
+++ js/index.js
@@ -1,29 +1,29 @@
 
 var images = [
-    "/images/images/Self_Portrait copy.png",
-    "/images/images/Self_Portrait 1.png",
-    "/images/images/Self_Portrait 1.png",
-    "/images/images/Self_Portrait 2.png",
-    "/images/images/Self_Portrait 3.png",
-    "/images/images/Self_Portrait 4.png",
-    "/images/images/Self_Portrait 5.png",
-    "/images/images/Self_Portrait 6.png",
-    "/images/images/Self_Portrait 7.png",
-    "/images/images/Self_Portrait 8.png",
-    "/images/images/Self_Portrait 9.png",
-    "/images/images/Self_Portrait 10.png",
-    "/images/images/Self_Portrait 11.png",
-    "/images/images/Self_Portrait 12.png",
-    "/images/images/Self_Portrait 13.png"
+    "/images/Self_Portrait copy.png",
+    "/images/Self_Portrait 1.png",
+    "/images/Self_Portrait 1.png",
+    "/images/Self_Portrait 2.png",
+    "/images/Self_Portrait 3.png",
+    "/images/Self_Portrait 4.png",
+    "/images/Self_Portrait 5.png",
+    "/images/Self_Portrait 6.png",
+    "/images/Self_Portrait 7.png",
+    "/images/Self_Portrait 8.png",
+    "/images/Self_Portrait 9.png",
+    "/images/Self_Portrait 10.png",
+    "/images/Self_Portrait 11.png",
+    "/images/Self_Portrait 12.png",
+    "/images/Self_Portrait 13.png"
 ];
 var currentIndex = 0;
 var container = $("#image-container");
 var image = $("<img>").addClass("image").appendTo(container);
 
 function showNextImage() {
-    image.attr("src", images[currentIndex]).fadeIn(1000, function() {
+    image.attr("src", images[currentIndex]).fadeIn(500, function() {
         currentIndex = (currentIndex + 1) % images.length;
-        setTimeout(showNextImage, 1000);
+        setTimeout(showNextImage, 500);
     });
 }
 
